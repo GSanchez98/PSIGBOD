0e5331d517173b4eb0f9c764932ad272e2b9e7eb
Gimena Sánchez <gmnsanchezicc@gmail.com>
2021-06-07 10:59:46 -0600

    Navegación primeros menús (Parte 1)

---
+++ FLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace SIGBOD
+{
+    public partial class FLogin : Form
+    {
+        public FLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConexionBD conexion = new();
+            conexion.Abrir();
+
+            //CODIGO PARA LISTAR
+            string cadena = "Select * from usuarios";
+            SqlCommand comando = new(cadena, conexion.conectarBD);
+            SqlDataAdapter adaptador = new SqlDataAdapter(comando);
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //***********************************************************************************************************************************//
+            // NOTA: SI INGRESA EL USUARIO Y CLAVE CORRECTA LLAMA AL MENU PRINCIPAL.
+            //***********************************************************************************************************************************//
+            FMenuInicial menui = new();
+            menui.Show();
+        }
+    }
+}
---
+++ FMConfiguracion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIGBOD
+{
+    public partial class FMConfiguracion : Form
+    {
+        public FMConfiguracion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnRes.Visible = true;
+            btnMax.Visible = false;
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            btnRes.Visible = false;
+            btnMax.Visible = true;
+        }
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRes.Visible = false;
+            btnMax.Visible = true;
+        }
+    }
+}
