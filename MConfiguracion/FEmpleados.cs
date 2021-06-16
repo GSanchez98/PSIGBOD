e992da51d9ad2bef857ba0432e157b4cac534d73
Gimena Sánchez <gmnsanchezicc@gmail.com>
2021-06-15 20:23:26 -0600

    Restablecimiento

--- MConfiguracion/FEmpleados.cs
+++ MConfiguracion/FEmpleados.cs
@@ -154,13 +154,13 @@ namespace SIGBOD
                     comando.Parameters.AddWithValue("@direccion_Empleado", txtDireccion.Text);
                     comando.Parameters.AddWithValue("@correo_Empleado", txtCorreo.Text);
                     comando.Parameters.AddWithValue("@id_cargo_Empleado", Convert.ToInt32(cmbCargo.SelectedValue));
-<<<<<<< HEAD:FEmpleados.cs
-                    comando.Parameters.AddWithValue("@fecha_nacimiento_Empleado", txtFechaNac.Text.ToShortDateString().ToString());
-                    comando.Parameters.AddWithValue("@fecha_ingreso_Empleado", txtFechaIng..ToShortDateString().ToString());
-=======
+
+                    comando.Parameters.AddWithValue("@fecha_nacimiento_Empleado", txtFechaNac.Value.ToShortDateString().ToString());
+                    comando.Parameters.AddWithValue("@fecha_ingreso_Empleado", txtFechaIng.Value.ToShortDateString().ToString());
+
                     comando.Parameters.AddWithValue("@fecha_nacimiento_Empleado", fechaNacimiento);
                     comando.Parameters.AddWithValue("@fecha_ingreso_Empleado", fechaIngreso);
->>>>>>> Gimena:MConfiguracion/FEmpleados.cs
+
                     comando.Parameters.AddWithValue("@salario_Empleado", txtSalario.Text);
                     comando.Parameters.AddWithValue("@Imagen_Empleado", txtRuta.Text);
                     comando.Parameters.AddWithValue("@estado_Empleado", 1);
